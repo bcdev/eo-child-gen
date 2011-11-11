e3c0454b1906e670c0f7bf3ea3879400dd8f25dd
Tom Block <tom.block@brockmann-consult.de>
2011-11-11 15:19:16 +0100

    intermediate

---
+++ geo-child-gen/src/test/java/com/bc/merci/geochildgen/GeoChildGenAcceptanceTest.java
@@ -0,0 +1,10 @@
+package com.bc.merci.geochildgen;
+
+import junit.framework.TestCase;
+
+public class GeoChildGenAcceptanceTest extends TestCase {
+
+    public void testSubsetFrom_AATSR() {
+
+    }
+}
